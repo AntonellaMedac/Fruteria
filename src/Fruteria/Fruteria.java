e577a5d93c0546bd9df905921e955878825982a9
Manuel <mdt0001@alu.medac.es>
2022-03-31 11:05:02 +0200

    Añado las propiedades de fruteria

--- src/Fruteria/Fruteria.java
+++ src/Fruteria/Fruteria.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Fruteria;
 
-/**
- *
- * @author manue
- */
+import java.util.ArrayList;
+
 public class Fruteria {
-    
+    private String fruteria;
+    private ArrayList<Cliente> listaClientesNoAtendidos;
+    private ArrayList<Cliente> listaClientesAtendidos;
+    private int contadorClientes;
 }
