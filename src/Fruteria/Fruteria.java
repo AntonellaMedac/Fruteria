4aa0e00779e3e87547a0a25d5afdc8a9c5238e79
Manuel <mdt0001@alu.medac.es>
2022-04-01 09:00:01 +0200

    Propiedades, constuctor, geters && setters, clase fruteria

--- src/Fruteria/Fruteria.java
+++ src/Fruteria/Fruteria.java
@@ -1,11 +1,51 @@
-
 package Fruteria;
 
 import java.util.ArrayList;
 
 public class Fruteria {
-    private String fruteria;
+
+    private String nombre;
     private ArrayList<Cliente> listaClientesNoAtendidos;
     private ArrayList<Cliente> listaClientesAtendidos;
     private int contadorClientes;
+
+    public Fruteria(String nombre) {
+        this.setFruteria(nombre);
+        this.listaClientesAtendidos = new ArrayList<>();
+        this.listaClientesNoAtendidos = new ArrayList<>();
+        this.setContadorClientes(0);
+    }
+
+    public String getFruteria() {
+        return nombre;
+    }
+
+    public void setFruteria(String fruteria) {
+        this.nombre = fruteria;
+    }
+
+    public ArrayList<Cliente> getListaClientesNoAtendidos() {
+        return listaClientesNoAtendidos;
+    }
+
+    public void setListaClientesNoAtendidos(ArrayList<Cliente> listaClientesNoAtendidos) {
+        this.listaClientesNoAtendidos = listaClientesNoAtendidos;
+    }
+
+    public ArrayList<Cliente> getListaClientesAtendidos() {
+        return listaClientesAtendidos;
+    }
+
+    public void setListaClientesAtendidos(ArrayList<Cliente> listaClientesAtendidos) {
+        this.listaClientesAtendidos = listaClientesAtendidos;
+    }
+
+    public int getContadorClientes() {
+        return contadorClientes;
+    }
+
+    public void setContadorClientes(int contadorClientes) {
+        this.contadorClientes = contadorClientes;
+    }
+
 }
